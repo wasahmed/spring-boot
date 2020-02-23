74b553937fdb6dd37d24ecda700bf0d1ed61e2cc
Waseem Ahmed <waseema@bbd.co.za>
2020-02-23 18:52:34 +0200

    GET, POST, PUT, DELETE

--- learningspring/src/main/java/com/example/learningspring/TopicService.java
+++ learningspring/src/main/java/com/example/learningspring/TopicService.java
@@ -3,6 +3,7 @@ package com.example.learningspring;
 import com.example.learningspring.model.Topics;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -10,11 +11,12 @@ import java.util.stream.Stream;
 @Service
 public class TopicService {
 
-    List<Topics> topics = Arrays.asList(
+    List<Topics> topics = new ArrayList<>(Arrays.asList(
             new Topics("1", "test", "test desc"),
             new Topics("2", "test1", "test desc1"),
             new Topics("3", "test2", "test desc2")
-    );
+    ));
+
 
     public List<Topics> getAllTopics()
     {
@@ -24,5 +26,25 @@ public class TopicService {
     public Topics getTopic(String id)
     {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topics topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topics topic, String id) {
+        for (int i = 0; i < topics.size(); i++)
+        {
+            Topics t = topics.get(i);
+            if (t.getId().equals(id))
+            {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
--- learningspring/src/main/java/com/example/learningspring/controller/controller.java
+++ learningspring/src/main/java/com/example/learningspring/controller/controller.java
@@ -4,9 +4,7 @@ import com.example.learningspring.TopicService;
 import com.example.learningspring.model.Topics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.lang.reflect.Array;
@@ -18,15 +16,36 @@ public class controller {
 
     @Autowired
     private TopicService topicService;
+    //instance of the service
 
     @RequestMapping("/topics")
     public List<Topics> getAllTopics()
     {
         return topicService.getAllTopics();
     }
+    //getting the list from our service
 
     @RequestMapping("/topics/{id}")
-    public Topics getTopic(@PathVariable String id){
+    public Topics getTopic(@PathVariable String id)
+    {
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topics topic)
+    {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "topics/{id}")
+    public void updateTopic(@RequestBody Topics topic, @PathVariable String id)
+    {
+        topicService.updateTopic(topic, id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "topics/{id}")
+    public void deleteTopic(@PathVariable String id)
+    {
+        topicService.deleteTopic(id);
     }
 }
